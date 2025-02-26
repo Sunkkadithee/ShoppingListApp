44c58621fd0ecd79ad68c22434b8cb92de8091eb
Pang Sunkkadithee <pang@Pangs-MacBook-Air.local>
2025-02-26 00:39:13 -0500

    Added shopping list file

---
+++ shopping list/shopping list/shopping_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopping_listApp.swift
+//  shopping list
+//
+//  Created by Pang Sunkkadithee on 2025-02-24.
+//
+
+import SwiftUI
+
+@main
+struct shopping_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
