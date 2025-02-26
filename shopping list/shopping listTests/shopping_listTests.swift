f9af089aadf00c3f63cd853030664e0f07d0a751
Pang Sunkkadithee <pang@Pangs-MacBook-Air.local>
2025-02-26 00:48:41 -0500

    Added file project

---
+++ shopping list/shopping listTests/shopping_listTests.swift
@@ -0,0 +1,17 @@
+//
+//  shopping_listTests.swift
+//  shopping listTests
+//
+//  Created by Pang Sunkkadithee on 2025-02-24.
+//
+
+import Testing
+@testable import shopping_list
+
+struct shopping_listTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
